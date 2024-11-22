5a8bc25e3a9bc57b569e858ac3bf8271333047a6
Sam Rose <samuel@supabase.io>
2024-11-22 13:37:27 -0500

    fix: cleanup seed

--- seed.sql
+++ seed.sql
@@ -20,7 +20,6 @@ SET row_security = off;
 -- Data for Name: packages; Type: TABLE DATA; Schema: public; Owner: -
 --
 
-INSERT INTO public.packages (id, attr_path, name, version, description, source_url, build_phase, install_phase, unpack_phase, source_hash) VALUES (2, 'stdenv', 'stdenv', '1.0', 'Standard build environment', NULL, NULL, NULL, NULL, NULL);
 INSERT INTO public.packages (id, attr_path, name, version, description, source_url, build_phase, install_phase, unpack_phase, source_hash) VALUES (1, 'hello-world', 'hello-world', '1.0.0', 'A basic Hello World program', 'https://gist.githubusercontent.com/samrose/3ac76cf99e7e0bea627823e30b9bdd92/raw/6545635aca025918c986301a7eef903034193544/hello.c', '$CC hello.c -o hello', 'mkdir -p $out/bin; cp hello $out/bin/', 'cp $src hello.c', 'sha256-n1VVXku/KHEg+HU0SZJlGvI5yrGhwaoYMehYTiPVE40=');
 
 
